eaef976f62bd7543ccd2cd8863f7d2041a2641f6
lndoub4@gmail.com <lndoub4@gmail.com>
2019-05-20 13:44:12 +0600

    1.3.1 Maven Plugins[#139740]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,4 +1,4 @@
-package ru.kudabaev;
+package ru.job4j.calculate;
 
 /**
  *Class Calculate решение задачи части 001 урок1.
@@ -6,13 +6,12 @@ package ru.kudabaev;
  *@since 17.05.2019
  */
 
+public class Calculate {
 /**
 * Метод для тестирования.
 * @param value строка для вывода в консоль
 * @return String Value.
 */
-
-public class Calculate {
 	public String echo(String value){
 	return String.format("%s %s %s", value, value, value);
 	}
--- chapter_001/src/main/java/ru/job4j/calculate/package-info.java
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -2,7 +2,23 @@
 * Package for calculate task.
 *
 * @author Rinat Kudabaev (kudabaevrm@gmail.com)
+* @version 0.1
+* @since 17.05.2019
+*/
+
+package ru.job4j.calculate;
+
+
+
+
+
+
+/*
+* Package for calculate task.
+*
+* @author Rinat Kudabaev (kudabaevrm@gmail.com)
 * @version $Id$
 * @since 0.1
-*/
-package ru.job4j.calculate;
+
+
+package ru.job4j.calculate;*/
