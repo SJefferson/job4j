3e0a144103690195117b494168f79397dc7d67c8
lndoub4@gmail.com <lndoub4@gmail.com>
2019-05-24 11:19:02 +0600

    Converter without mistakes

---
+++ chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConverterTest {
+    @Test
+    public void when60RubleToDollarThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToDollar(60);
+        int expected = 1;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void when70RubleToEuroThen1() {
+        Converter converter = new Converter();
+        int result = converter.euroToRuble(70);
+        int expected = 1;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void when1DollarThen60Rubles() {
+        Converter converter = new Converter();
+        int result = converter.dollarToRuble(1);
+        int expected = 60;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void when1EuroThen70Rubles() {
+        Converter converter = new Converter();
+        int result = converter.rubleToEuro(70);
+        int expected = 1;
+        assertThat(result, is(expected));
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/converter/package-info.java
@@ -0,0 +1,13 @@
+package ru.job4j.converter;
+
+ /*
+ * Package for converter task.
+ *
+ * @author Rinat Kudabaev (kudabaevrm@gmail.com)
+ * @version 0.1
+ * @since 24.05.2019
+ */
+
+
+
+
