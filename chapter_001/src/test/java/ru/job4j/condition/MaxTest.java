a52221463578586ad9ad53d333d69ae4267e141e
lndoub4@gmail.com <lndoub4@gmail.com>
2019-05-27 14:59:29 +0600

    Max 0.2

--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -5,9 +5,23 @@ import static org.junit.Assert.assertThat;
 
 public class MaxTest {
     @Test
-    public void whenMax1To2Then2() {
+    public void whenMax1To1Then2() {
         Max max = new Max();
         int result = max.max(1, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenMax1To2Then1() {
+        Max max = new Max();
+        int result = max.max(2, 1);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenMax1To2Then2() {
+        Max max = new Max();
+        int result = max.max(2, 2);
         assertThat(result, is(2));
     }
 }
