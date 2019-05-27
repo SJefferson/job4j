96368290e089f31e2c7dd4690744d7123bdd7e04
lndoub4@gmail.com <lndoub4@gmail.com>
2019-05-27 13:45:51 +0600

    DummyBot

--- chapter_001/src/main/java/ru/job4j/sqarea/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/sqarea/SqArea.java
@@ -6,25 +6,25 @@ package ru.job4j.sqarea;
 
 public class SqArea {
 
-    /**
+    /*
      * Расчитываем ширину.
-     * @param per периметр.
+    * @param per периметр.
      * @param k коэфициент.
      * @return ширина.
      */
-    public int with(int per, int k) {
+/*    public int with(int per, int k) {
         return per / (2 + (2 * k));
-    }
+    }*/
 
-    /**
+    /*
      * Расчитываем длину.
      * @param per периметр.
      * @param k коэфициент.
      * @return ширина.
      */
-        public int height(int per, int k) {
+/*        public int height(int per, int k) {
         return per * k / (2 + (2 * k));
-    }
+    }*/
 
     /**
      * Расчитываем длину.
---
+++ chapter_001/src/test/java/ru/job4j/square/package-info.java
@@ -0,0 +1,9 @@
+package ru.job4j.square;
+
+/*
+ * Package for square task.
+ *
+ * @author Rinat Kudabaev (kudabaevrm@gmail.com)
+ * @version 0.1
+ * @since 24.05.2019
+ */
