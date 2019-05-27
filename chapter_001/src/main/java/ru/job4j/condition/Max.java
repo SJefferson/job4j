34028629bfaad9fbcd71d02794b2d6c79af5fb90
lndoub4@gmail.com <lndoub4@gmail.com>
2019-05-27 14:10:59 +0600

    Max

---
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,9 @@
+package ru.job4j.condition;
+
+public class Max {
+    public int max(int first, int second) {
+     int result;
+     result = first > second ? first : second;
+     return result;
+    }
+}
