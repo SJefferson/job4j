51bf1cfc8675169bee8cbd7fef59e5b422266277
lndoub4@gmail.com <lndoub4@gmail.com>
2019-06-05 15:16:14 +0600

    FindLoop

---
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.array;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+
+
+    @Test
+    public void whenArrayHas13Then6() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {2, 4, 6, 7, 4, 15, 13};
+        int value = 13;
+        int result = find.indexOf(input, value);
+        int expect = 6;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHas77Then3() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {2, 4, 78, 77};
+        int value = 77;
+        int result = find.indexOf(input, value);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+}
