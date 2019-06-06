c0d3e9565a8949928da255c3b732856caa431927
lndoub4@gmail.com <lndoub4@gmail.com>
2019-06-06 13:53:14 +0600

    Комментарии 5.2, 6.1, 6.2 исправлены, можно закрывать?

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -3,14 +3,11 @@ package ru.job4j.loop;
 public class Factorial {
     public int calc(int n) {
         int result = 1;
-        if (n != 0) {
             for (int i = 1; i <= n; i++) {
                 result = result * i;
             }
-        } else {
-            result = 1;
-        }
-
         return result;
     }
+
 }
+
