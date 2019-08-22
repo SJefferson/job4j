93d8712408f3083b642c949796861bfd1b9c8d6f
lndoub4@gmail.com <lndoub4@gmail.com>
2019-08-22 08:33:37 +0600

    Point (distance between points)

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -24,9 +24,6 @@ public class Point {
         this.y = second;
     }
 
-    public Point() {
-    }
-
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
